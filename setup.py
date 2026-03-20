71c06c7f75ed6b847e4047caf6c1638b909ae1e4
Alessio Santoru <alessiosantoru@gmail.com>
2026-03-20 12:15:04 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,42 +1,13 @@
 """ Tool Setup """
 # !/usr/bin/env python3
 
-import os
-from shutil import copyfile
 from setuptools import setup
-from setuptools.command.install import install
 
 PACKAGE_NAME = "pihole-dashboard"
-VERSION = "2.0.10"  # Updated for Pi-hole v6 compatibility
+VERSION = "2.0.10"
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
-
-
-def parse_requirements(filename):
-    """Load requirements from a pip requirements file."""
-    lineiter = (line.strip() for line in open(filename))
-    return [line for line in lineiter if line and not line.startswith("#")]
-
-
-class PostInstallJob(install):
-    """Post-installation for installation mode."""
-    def run(self):
-        install.run(self)
-        print("Installing cronjob...")
-        self_dir = os.path.dirname(os.path.realpath(__file__))
-        cron_dir = os.path.join(self_dir, 'cron')
-        copyfile(os.path.join(cron_dir, "pihole-dashboard-cron"),
-                 "/etc/cron.d/pihole-dashboard-cron")
-        print("Installing config file...")
-        self_dir = os.path.dirname(os.path.realpath(__file__))
-        conf_dir = os.path.join(self_dir, 'conf')
-        conf_sys_dir = "/etc/pihole-dashboard"
-        if not os.path.exists(conf_sys_dir):
-            os.makedirs(conf_sys_dir)
-        copyfile(os.path.join(conf_dir, "config.toml"),
-                 "/etc/pihole-dashboard/config.toml")
-        print("Done.")
 
 
 if __name__ == "__main__":
@@ -50,7 +21,13 @@ if __name__ == "__main__":
         long_description_content_type="text/markdown",
         url="https://github.com/santoru/pihole-dashboard",
         packages=["pihole_dashboard"],
-        package_data={'pihole_dashboard': ['font/*.ttf']},
+        package_data={
+            'pihole_dashboard': ['font/*.ttf'],
+        },
+        data_files=[
+            ('/etc/cron.d', ['cron/pihole-dashboard-cron']),
+            ('/etc/pihole-dashboard', ['conf/config.toml']),
+        ],
         scripts=[
             "scripts/pihole-dashboard-clean-screen",
             "scripts/pihole-dashboard-draw"
@@ -62,7 +39,4 @@ if __name__ == "__main__":
             "Pillow>=8.2.0",
             "toml>=0.10.2"
         ],
-        cmdclass={
-            'install': PostInstallJob,
-        },
     )
